caae238813dc680a1bf5196264458e90012b0a1d
Yurchenko Stanislav <yurchenko.stanislav@ukr.net>
2021-08-08 22:19:24 +0300

    no connect to mongo db

--- src/backend/bin/server.js
+++ src/backend/bin/server.js
@@ -5,12 +5,13 @@ const { createDirIfIsNotExist } = require('../utils/createDir')
 const { DIRS } = require('../utils/constants')
 
 const PORT = process.env.PORT || 3000
+const HOST = process.env.HOST || '0.0.0.0'
 
 const serverInit = async() => {
   await createDirIfIsNotExist(DIRS.upload)
   await createDirIfIsNotExist(DIRS.public)
   await createDirIfIsNotExist(DIRS.avatar)
-  app.listen(PORT, () => {
+  app.listen(PORT, HOST, () => {
     console.log(`<<< Server running. Use our API on port: http://localhost:${PORT} >>>`)
   })
 }
